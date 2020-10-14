3d0ad5807858c58a323b10594e9ad1d615b26e26
estacio <henrye@unicauca.edu.co>
2020-10-13 19:41:27 -0500

    algo

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import socket
 
 HOST = "127.0.0.1"
-PORT = 65432
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-print("conexion establecidad con el servidor")
+PORT = 7879
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    print("conexion establecidad con el servidor")
 
-while True:
-    data = s.recv(1024).decode()
-    if not data:
-        break
-    print("Mensaje recibido del servidor:", data)
-    mensaje_enviar = str(input())
-    s.sendall(mensaje_enviar.encode())
+    while True:
+        data = s.recv(1024).decode()
+        if not data:
+            break
+        print("Mensaje recibido del servidor:", data)
+        mensaje_enviar = input()
+        s.sendall(mensaje_enviar.encode())
--- servidor.py
+++ servidor.py
@@ -1,17 +1,66 @@
 import  socket
 
 with socket.socket (socket.AF_INET, socket.SOCK_STREAM) as so:
-    so.bind(('127.0.0.1', 9191))
+    so.bind(('127.0.0.1', 7879))
     so.listen()
     print('servidor encendido, esperando conexion...')
     conexion, direccion = so.accept()
     with conexion:
         print('conexion establecidad desde: ', direccion)
-        while True:
-            # ----- envio de informacion----
-            datos_recividos = conexion.recv(1024)
-            if not datos_recividos:
-                break
-            print('he recivido datos desde el cliente: ', datos_recividos.decode())
-            #recibi la infromacion de cliente y le voy a enviar un saludo
-            conexion.sendall(b'hola desde el servidor')
+
+        ingreso = 0
+            # ingreso = int(input())
+        while ingreso != 2:
+            conexion.sendall("1: calcular masa, \n2: salir del programa".encode())
+
+            try:
+                print("algo")
+                ingreso = so.recv(1024).decode()
+                ingreso1 = str(ingreso)
+                ingreso2 = int(ingreso1)
+                if ingreso2 == 1:
+                    ban = True
+                    while ban == True:
+
+                        try:
+                            conexion.sendall("ingresar altura en metro".encode())
+                            altura = so.recv(1024).decode()
+                            altura2 = float(altura)
+                            conexion.sendall("ingresar peso".encode())
+                            peso = so.recv(1024).decode()
+                            peso2 = float(peso)
+                            indice = peso2 / (altura2 * altura2)
+                            indice2 = str(indice)
+                            conexion.sendall(indice2.encode())
+                            ban = False
+                        except ValueError:
+                            conexion.sendall("elija una opcion valida".encode())
+                        except ZeroDivisionError:
+                            conexion.sendall("la altura no puede ser 0".encode())
+
+                        if indice < 18.5:
+                            conexion.sendall("usted se encuentra bajo de peso".encode())
+
+                        if indice > 18.5 and indice < 24.9:
+                            conexion.sendall("se encuentra en un estado normal".encode())
+
+                        if indice > 25 and indice < 29.9:
+                            conexion.sendall("se encuentra en sobre peso".encode())
+
+                        if indice > 30 and indice < 34.9:
+                            conexion.sendall("se encuentra en obesidad 1".encode())
+
+                        if indice > 35 and indice < 39.9:
+                            conexion.sendall("se encuentra en obesidad 2".encode())
+
+                        if indice > 40 and indice < 49.9:
+                            conexion.sendall("se encuentra en obesidad 3".encode())
+
+                        if indice > 50:
+                            conexion.sendall("se encuentra en obesidad 4".encode())
+
+
+
+            except ValueError:
+                print("elija una opcion valida")
+
